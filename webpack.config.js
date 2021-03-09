c4cdd47aa5920dcb7bee53363eb07aa05c96eceb
MateusJesus <mateusjdev@gmail.com>
2021-03-09 15:11:40 -0300

    :sparkles: Typescript in ReactJS

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
   devtool: isDevelopment ? 'eval-source-map' : 'source-map',
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
@@ -34,7 +34,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        // meus componentes podem estar escritos tanto em jsx como tsx "| = ou"
+        test: /\.(j|t)sx$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
